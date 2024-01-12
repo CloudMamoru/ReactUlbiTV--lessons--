da7eb5d710897c6060254bbb5117cc7903b1bd6c
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2024-01-12 17:16:46 +0300

    Компонент App. Работа с состоянием. useState.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { useState } from 'react';
+
 function App() {
+  let [likes, setLikes] = useState(0);
   return (
     <div className='App'>
-      <div></div>
+      <div>
+        <h1>{likes}</h1>
+        <button onClick={() => setLikes(likes + 1)}>Increment</button>
+        <button onClick={() => setLikes(likes - 1)}>Decrement</button>
+      </div>
     </div>
   );
 }
