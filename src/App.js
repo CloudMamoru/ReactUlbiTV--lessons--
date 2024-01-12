5fbdafb5a6dff9e0d6a35968b919c5ec5cb032f1
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2024-01-12 17:40:26 +0300

    Управляемый инпут

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { useState } from 'react';
 
 function App() {
-  let [likes, setLikes] = useState(0);
+  const [likes, setLikes] = useState(0);
+  const [value, setValue] = useState('Text in <input>');
   return (
     <div className='App'>
       <div>
         <h1>{likes}</h1>
         <button onClick={() => setLikes(likes + 1)}>Increment</button>
         <button onClick={() => setLikes(likes - 1)}>Decrement</button>
+        <h1>{value}</h1>
+        <input type='text' value={value} onChange={(e) => setValue(e.target.value)} />
       </div>
     </div>
   );
