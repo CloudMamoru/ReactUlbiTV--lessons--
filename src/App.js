eb549fb3417abf3c75d6f7488f1dfc98e1847946
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2024-01-12 20:48:18 +0300

    Хук useRef. Доступ к DOM-элементу. Неуправляемый компонент

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import './styles/App.css';
 import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
@@ -26,29 +26,26 @@ function App() {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
 
+  const bodyInputRef = useRef();
+
   const addNewPost = (e) => {
     // Предотвращает обновления страницы при использования кнопки (предотвращает поведение по умолчанию)
     e.preventDefault();
-    setPosts([...posts, { id: 4, title, body }]);
-    setTitle('');
-    setBody('');
+    setPosts([...posts, { id: 4, title, body: bodyInputRef.current.value }]);
   };
 
   return (
     <div className='App'>
       <form>
+        {/* Управляемый компонент */}
         <MyInput
           type='text'
           value={title}
           onChange={(e) => setTitle(e.target.value)}
           placeholder='Название поста'
         />
-        <MyInput
-          type='text'
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
-          placeholder='Содержание поста'
-        />
+        {/* Неуправляемый/Неконтролируемый компонент */}
+        <MyInput ref={bodyInputRef} type='text' placeholder='Содержание поста' />
         <MyButton onClick={addNewPost}>Создать пост</MyButton>
       </form>
       <PostList posts={posts} title={'Список постов'} />
--- src/components/UI/input/MyInput.jsx
+++ src/components/UI/input/MyInput.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styles from './MyInput.module.css';
 
-const MyInput = (props) => {
-  return <input {...props} className={styles.myInp} />;
-};
+const MyInput = React.forwardRef((props, ref) => {
+  return <input ref={ref} {...props} className={styles.myInp} />;
+});
 
 export default MyInput;
