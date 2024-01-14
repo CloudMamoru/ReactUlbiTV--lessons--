9451fc993350fac079d661c3ebb5f44d23b4de99
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2024-01-14 19:04:02 +0300

    Анимация. React transition group

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PostItem from './PostItem';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 // Применени деструкторизации пропсов
 const PostList = ({ deletePost, posts, title }) => {
@@ -9,9 +10,13 @@ const PostList = ({ deletePost, posts, title }) => {
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>{title}</h1>
-      {posts.map((p, index) => (
-        <PostItem index={index + 1} deletePost={deletePost} key={p.id} post={p} />
-      ))}
+      <TransitionGroup>
+        {posts.map((p, index) => (
+          <CSSTransition key={p.id} timeout={500} classNames='post'>
+            <PostItem index={index + 1} deletePost={deletePost} post={p} />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </div>
   );
 };
